2872095e82cb2d3acb486ac9cdd5a1e7def589fd
pedrorossato <phrossato@inf.ufsm.br>
2020-05-22 11:59:01 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {WebView} from 'react-native-webview';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <WebView source={{uri: 'http://painel.ascendo.com.br'}} />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  center: {
+    alignItems: 'center',
+  },
+});
+
+export default App;
